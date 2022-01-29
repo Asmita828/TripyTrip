76a336cfaaf57a4cee13f5a2a00151aee1bad920
Asmita828 <asmitagoyal828@gmail.com>
2022-01-30 00:16:27 +0530

    campground index

--- app.js
+++ app.js
@@ -10,31 +10,20 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
     .then(() => {
         console.log("Connection created")
-
+        app.listen(3000, () => {
+            console.log('Connection created!!')
+        })
     })
     .catch((err) => {
         console.log("OOPs error found!!");
         console.log(err);
     })
-// mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-//     // useNewUrlParser: true,
-//     //userCreateIndex: true,
-//     // useUnifiedTopology: true
-// });
-// const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "connection error:"));
-// db.once("open", () => {
-//     console.log("Database Connected!!");
-// });
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 app.get('/campground', async (req, res) => {
-    const camp = new Campground({ title: 'My Backyard' });
-    await camp.save();
-    res.send(camp);
+    const campgrounds = await Campground.find({});
+    res.render('campground/index', { campgrounds })
 })
-app.listen(3000, () => {
-    console.log('Connection created!!')
-})
+
