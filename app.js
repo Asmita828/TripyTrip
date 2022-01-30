080f54524833b9d4305597532d099e8ef81b0119
Asmita828 <asmitagoyal828@gmail.com>
2022-01-30 15:01:15 +0530

    campground edit and update

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 
-// var methodOverride = require('method-override')
-// app.use(methodOverride('_method'))
+var methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 
 const Campground = require('./models/campground')
 
 const mongoose = require('mongoose');
+const { findByIdAndUpdate } = require('./models/campground')
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
     .then(() => {
         console.log("Connection created")
@@ -45,5 +46,17 @@ app.get('/campgrounds/:id', async (req, res) => {
     //console.log(req.params)
     const campground = await Campground.findById(req.params.id);
     res.render('campground/show', { campground })
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    //console.log(req.params)
+    const campground = await Campground.findById(req.params.id);
+    res.render('campground/edit', { campground })
 })
 
