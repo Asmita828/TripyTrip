a896a11f68221e9d834203028dc166e71d4b7915
Asmita828 <asmitagoyal828@gmail.com>
2022-02-05 13:59:45 +0530

    Defining error template

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError')
 
 app.use(express.urlencoded({ extended: true }))
 
@@ -16,7 +17,7 @@ app.use(methodOverride('_method'))
 const Campground = require('./models/campground')
 
 const mongoose = require('mongoose');
-const { findByIdAndUpdate } = require('./models/campground')
+const { findByIdAndUpdate } = require('./models/campground');
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
     .then(() => {
         console.log("Connection created")
@@ -38,6 +39,7 @@ app.get('/campgrounds', catchAsync(async (req, res) => {
 }))
 
 app.post('/campgrounds', catchAsync(async (req, res) => {
+    if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400)
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`campgrounds/${campground._id}`)
@@ -70,6 +72,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds')
 }))
 
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found!!', 404))
+})
+
 app.use((err, req, res, next) => {
-    res.send("Something went wrong!!");
+    const { statusCode = 500 } = err;
+    if (!err.message) {
+        err.message = 'Oh Nooo! Something went wrong!!'
+    }
+    //console.log(err)
+    res.status(statusCode).render('error', { err });
 })
