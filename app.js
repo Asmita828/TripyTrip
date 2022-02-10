91d7cb6f4668a519fd9af3cd2b3615b14775c655
Asmita828 <asmitagoyal828@gmail.com>
2022-02-10 23:28:55 +0530

    Configuring Session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const ejsMate = require('ejs-mate')
+const session = require('express-session')
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -31,6 +32,18 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp')
         console.log("OOPs error found!!");
         console.log(err);
     })
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + (1000 * 60 * 60 * 24 * 7),
+        maxAge: (1000 * 60 * 60 * 24 * 7)
+    }
+}
+app.use(session(sessionConfig))
 
 app.get('/', (req, res) => {
     res.render('home')
